c56bf829e3d99e1a6908930a5d2d97d14fc46153
ashatkydyrov61@gmail.com <ashatkydyrov61@gmail.com>
2023-03-10 19:10:45 -0600

    dz6

---
+++ bank.py
@@ -0,0 +1,82 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.uic import loadUi
+import sys
+import sqlite3
+import time
+
+class StartDB:
+    def __init__(self):
+        self.connect = sqlite3.connect('bank.db')
+        self.connect.execute("""
+            CREATE TABLE IF NOT EXISTS users(
+            login VARCHAR(255),
+            password VARCHAR(255),
+            email VARCHAR(255),
+            created VARCHAR(100)
+            );
+        """)
+        self.connect.commit()
+
+class SignUp(QWidget):
+    def __init__(self):
+        super(SignUp, self).__init__()
+        loadUi('signup.ui', self)
+        self.hide_error()
+        self.db = StartDB()
+
+    def hide_error(self):
+        self.error.hide()
+
+    def show_error(self):
+        self.error.show()
+
+    def register(self):
+        login = self.login.text()
+        password = self.password.text()
+        email = self.email.text()
+        self.show_error()
+        cursor = self.db.connect.cursor()
+        try:
+            cursor.execute(f"INSERT INTO users VALUES ('{login}', '{password}', '{email}', '{time.ctime()}');")
+            self.error.setText("Успешно")
+        except sqlite3.IntegrityError as s:
+            print(s.args)
+            if s.args == "('UNIQUE constraint failed: users.login',)":
+                self.error.setText("Логин уже занят.")
+            elif s.args == ('UNIQUE constraint failed: users.email',):
+                self.error.setText("Почта уже занята.")
+            else:
+                self.error.setText("Логин занят.")
+        self.db.connect.commit()
+class Bank(QMainWindow):
+    def __init__(self):
+        super(Bank, self).__init__()
+        loadUi('main.ui', self)
+        self.hide_error()
+        self.signin.clicked.connect(self.check_login)
+        self.class_signup = SignUp()
+        self.signup.clicked.connect(self.show_signup)
+        
+    def show_signup(self):
+        self.class_signup.show()
+
+    def hide_error(self):
+        self.error.hide()
+
+    def show_error(self):
+        self.error.show()
+
+    def check_login(self):
+        login = self.login.text()
+        password = self.password.text()
+        if login == 'geeks' and password == 'geeks2023':
+            self.show_error()
+            self.error.setText("Ok")
+        else:
+            self.show_error()
+            self.error.setText("Неправильные данные")
+
+app = QApplication(sys.argv)
+bank = Bank()
+bank.show()
+app.exec_()
